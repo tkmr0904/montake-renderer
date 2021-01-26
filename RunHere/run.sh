f95d856bc341bc89480e78ee5658c4bebbc1c1fc
tkmr0904 <tkmr0904@gmail.com>
2021-01-26 15:57:43 +0900

    run.sh modified

--- RunHere/run.sh
+++ RunHere/run.sh
@@ -1,3 +1,6 @@
-cmake ../
-make -s -j 8
-./Source/a.out
+cmake ../ && make -s -j 8 && ./Source/a.out
+rm -f -r ./CMakeFiles
+rm -f -r ./Source
+rm ./cmake_install.cmake
+rm ./CMakeCache.txt
+rm ./Makefile
