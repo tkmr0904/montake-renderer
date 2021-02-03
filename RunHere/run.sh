79d01bb481c1269c2985b42bc39c3164776582bc
tkmr0904 <tkmr0904@gmail.com>
2021-02-03 20:08:49 +0900

    Directory "Source" was renamed to "src"

--- RunHere/run.sh
+++ RunHere/run.sh
@@ -1,6 +1,6 @@
-cmake ../ && make -s -j 8 && ./Source/a.out
+cmake ../ && make -s -j 8 && ./src/a.out
 rm -f -r ./CMakeFiles
-rm -f -r ./Source
+rm -f -r ./src
 rm ./cmake_install.cmake
 rm ./CMakeCache.txt
 rm ./Makefile
